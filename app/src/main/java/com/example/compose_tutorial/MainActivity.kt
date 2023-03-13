89dcd1a022d5bc6d02e9a692b829514b6eff923e
Aluno <nicotha2018@gmail.com>
2023-03-13 08:41:19 -0300

    versão 2 / 13-03-23

--- app/src/main/java/com/example/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/compose_tutorial/MainActivity.kt
@@ -3,33 +3,53 @@ package com.example.compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.compose_tutorial.ui.theme.Compose_TutorialTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello world!")
+            MessageCard(Message("Android", "JetpackCompose"))
         }
     }
-
 }
-    @Composable
-        fun MessageCard(name: String){
-            Text(text = "Hello $name!")
-    }
-    @Preview
-    @Composable
-        fun PreviewMessageCard(){
-            MessageCard("Android" )
+ data class Message(val author: String, val body: String)
 
+@Composable
+fun MessageCard(msg: Message){
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(id =R.drawable.hello_kitty2),
+            contentDescription = "Contact profile picture", 
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
     }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
+}
 
 
